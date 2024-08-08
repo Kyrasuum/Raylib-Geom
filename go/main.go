2e81844cb151ad14038e16b524bfb753376c2e65
phil <plorentz7@gmail.com>
2024-08-07 22:03:52 -0400

    got grass working

--- go/main.go
+++ go/main.go
@@ -23,8 +23,7 @@ func main() {
 		// Output vertex attributes (to geometry shader)
 		out vec4 geomColor;
 
-		void main()
-		{
+		void main() {
 		    // Send vertex attributes to fragment shader
 		    geomColor = vertexColor;
 
@@ -36,22 +35,43 @@ func main() {
 
 		layout (triangles) in;
 		layout (triangle_strip) out;
-		layout (max_vertices = 3) out;
+		layout (max_vertices = 256) out;
 
 		in vec4 geomColor[];
 
 		out vec4 fragColor;
 
-		void main()
-		{
+		// Input uniform values
+		uniform mat4 mvp;
+		uniform mat4 matModel;
+		uniform mat4 matView;
+		uniform mat4 matProjection;
+
+		void main() {
 		    int i;
-		    for(i = 0; i < gl_in.length(); i++)
-		    {
-		        gl_Position = gl_in[i].gl_Position;
-		        fragColor = geomColor[i];
-		        EmitVertex();
-		    }
-		    EndPrimitive();
+	    	int j;
+
+		    for (i = 0; i+2 < gl_in.length(); i+=3) {
+				for (j = 0; j < 64; j++) {
+		            vec3 randomOffset = vec3(
+		                fract(sin(dot(vec3(j, i, 0), vec3(12.9898, 78.233, 54.53))) * 43758.5453),
+		                0.0,
+		                fract(sin(dot(vec3(j + 1, i + 1, 0), vec3(12.9898, 78.233, 54.53))) * 43758.5453)
+		            );
+	    			vec4 grassPos = mix(mix(gl_in[i].gl_Position, gl_in[i+1].gl_Position, randomOffset.x), gl_in[i+2].gl_Position, randomOffset.z);
+
+	    			gl_Position = grassPos + matProjection * (matView * matModel * vec4(0,0,0,0) + vec4(0.02, 0, 0, 0));
+   					fragColor = vec4(0,0.4,0,1);
+	    			EmitVertex();
+	    			gl_Position = grassPos + mvp * vec4(0, 0.2, 0, 0) +  + matProjection * (matView * matModel * vec4(0,0,0,0) + vec4(0, 0.1, 0, 0));
+   					fragColor = vec4(0,0.5,0,1);
+	    			EmitVertex();
+	    			gl_Position = grassPos + matProjection * (matView * matModel * vec4(0,0,0,0) + vec4(-0.02, 0, 0, 0));
+   					fragColor = vec4(0,0.4,0,1);
+	    			EmitVertex();
+  						EndPrimitive();
+				}
+			}
 		}`
 	fragCode := `
 	    #version 330
@@ -60,8 +80,7 @@ func main() {
 
 	    out vec4 finalColor;
 
-	    void main()
-	    {
+	    void main() {
 	        finalColor = fragColor;
 	    }`
 
@@ -85,14 +104,13 @@ func main() {
 	obj.Materials.Shader = shader
 
 	position := rl.NewVector3(0.0, 0.0, 0.0) // Set model position
-
 	//----------------------------------------------------------------------------------
 	// Set render cycle to 60 fps
 	rl.SetTargetFPS(60)
-	rl.UpdateCamera(&camera, rl.CameraFirstPerson)
 	for !rl.WindowShouldClose() {
 		// Draw
 		//----------------------------------------------------------------------------------
+		rl.UpdateCamera(&camera, rl.CameraFirstPerson)
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 
